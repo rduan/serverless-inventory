df89825a23f67fbf7fbe2455e84074619a9d3f7c
Richard Duan <dxloader@gmail.com>
2018-08-10 21:58:03 +1000

    test hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, { API } from 'aws-amplify';
 import aws_exports from './aws-exports';
 
+Amplify.configure(aws_exports);
+
+let apiName = 'sampleCloudApi';
+let path = '/items';
+
 class App extends Component {
+  componentDidMount() {
+    API.get(apiName, path).then((response) => {
+      console.log(response)
+    });
+  }
+  
   render() {
     return (
       <div className="App">
